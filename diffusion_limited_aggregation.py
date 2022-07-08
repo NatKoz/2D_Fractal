9d131d3a3a8e950fdf1a9b2acb82175c58e954d2
NatKoz <63883750+NatKoz@users.noreply.github.com>
2022-07-08 13:31:07 +0200

    add random direction to diffusion limited aggregation in fractal structure

--- diffusion_limited_aggregation.py
+++ diffusion_limited_aggregation.py
@@ -1,8 +1,21 @@
 from area import Area
+from typing import Tuple, List
+import random
 
 class DiffusionLimitedAggregation:
-    def __init__(self, area: Area, particles_count: int = 10) -> None:
+    def __init__(self, area: Area, particles_count: int = 100) -> None:
         self.area = area
         self.x, self.y = self.area.get_random_boundary_coordinate()
         self.particles_count = particles_count
         self.remaining_particles = self.particles_count
+
+    def __get_random_direction(self) -> Tuple[int, int]:
+        return random.choice(((1, 0),
+                              (0, 1),
+                              (-1, 0),
+                              (0, -1),
+                              (1, 1),
+                              (1, -1),
+                              (-1, -1),
+                              (-1, 1)
+                              ))
