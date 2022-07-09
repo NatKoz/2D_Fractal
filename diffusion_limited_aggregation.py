65147f29ea5b9bb11b7724c0e327eee8ab4a665c
NatKoz <63883750+NatKoz@users.noreply.github.com>
2022-07-09 10:08:12 +0200

    add simulate to diffusion limited aggregation in fractal structure

--- diffusion_limited_aggregation.py
+++ diffusion_limited_aggregation.py
@@ -52,3 +52,27 @@ class DiffusionLimitedAggregation:
                or coordinates[1] == self.area.lattice.max_y - 1 \
                or coordinates[0] == self.area.lattice.min_x + 1 \
                or coordinates[1] == self.area.lattice.min_y + 1
+               
+                  
+    def simulate(self) -> None:
+        self.area.stick_particle(self.area.get_center())
+
+        while self.remaining_particles:
+            particle_coordinates = self.__get_random_step()
+
+            if self.area.particle_exists(
+                    (particle_coordinates[0], particle_coordinates[1])):
+                self.area.stick_particle((self.x, self.y))
+
+                if self.area.lattice and self.__is_lattice_reached(
+                        (self.x, self.y)):
+                    self.area.increase_lattice_size()
+
+                self.x, self.y = self.area.get_random_boundary_coordinate()
+                self.remaining_particles -= 1
+
+                print(self.remaining_particles)
+
+            else:
+                self.x, self.y = particle_coordinates
+
